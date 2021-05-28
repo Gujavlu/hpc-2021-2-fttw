4fce9d7f0cd26d550a7838e4f9d76efd2d3be707
jruiz971 <juanluisruiz971@gmail.com>
2021-05-28 18:06:40 -0500

    Code to check that the data read in the C program is correct.
    Test phase, you can ignore this code.

---
+++ Read_binary_file.py
@@ -0,0 +1,11 @@
+"""
+Code to check that the data read in the C program is correct. 
+
+Test phase, you can ignore this code.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+samples = np.fromfile('samples.iq', np.complex64) # Read in file.  We have to tell it what format it is
+for i in range(15):
+    print(samples[i].real)
