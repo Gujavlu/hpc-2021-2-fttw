a01576ee9a72cb50af17637b10f9da02a0d8be25
jruiz971 <juanluisruiz971@gmail.com>
2021-05-28 15:42:16 -0500

    Guarda los datos en señales reales e imaginarias
    Recibe y gusrda las señales separándolas en dos archivos de text, uno para los reales y otro para los imaginarios.
    frecuencia_central.- 96.7MHz
    sdr.sample_rate =  230000 #230KHz
    sdr.freq_correction = 1   # PPM
    sdr.gain = 'auto'Sample_rate = 230KHz

--- read_print_samples.py
+++ read_print_samples.py
@@ -7,7 +7,11 @@ import sys
 #def read_print_samples(sample_rate, center_freq, freq_correction):
 def read_print_samples(center_freq):
     """
+    input:
     center_freq.- Get/Set the center frequency of the device (in Hz)
+
+    output:
+    samples.- numpy array
     """
     sdr = RtlSdr()
     
@@ -28,10 +32,15 @@ def read_print_samples(center_freq):
     sdr.freq_correction = 1   # PPM
     sdr.gain = 'auto'
     
-    samples = sdr.read_samples(1024) #number  of  samples  or  bytes  to  read 
-    np.save('samples.npy', samples)
-    #print (samples)
+    samples = sdr.read_samples(512) #number  of  samples  or  bytes  to  read 
+
+    np.savetxt('samples_real.txt', samples.real, fmt='%10.15f' )
+    np.savetxt('samples_imag.txt', samples.imag, fmt='%10.15f' )
+    ##Por ahora lo voy a guardar como txt, pero seguiré en la busqueda de un formato mejor para la transferencia de archivos. 
+    ##npy parecía buena opción
+    #np.save('samples.npy',samples)
+
 
 if __name__=="__main__":
     #print(read_print_samples( int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]) ) )
-    read_print_samples( int(sys.argv[1]) ) #Frecuancia centra (estación de radio)
+    read_print_samples( int(sys.argv[1]) ) #Frecuencia central (estación de radio[Hz])
