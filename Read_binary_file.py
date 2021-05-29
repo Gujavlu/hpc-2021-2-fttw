e8450f960df992fe7439bf8e3040d7a695b7417b
jruiz971 <juanluisruiz971@gmail.com>
2021-05-28 19:44:04 -0500

    Check both real and imaginary data
    Auxiliar to check if C program reads well the binary file

--- Read_binary_file.py
+++ Read_binary_file.py
@@ -8,4 +8,5 @@ import matplotlib.pyplot as plt
 
 samples = np.fromfile('samples.iq', np.complex64) # Read in file.  We have to tell it what format it is
 for i in range(15):
-    print(samples[i].real)
+    #print(samples[i].real)
+    print("Real:{0} -- Imaginary:{1}".format(samples[i].real,samples[i].imag) )
