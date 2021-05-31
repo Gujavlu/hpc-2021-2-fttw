c85977ce51c2e1b4aa9565be81fb1e517a1df789
javo <javojavojavojavo@gmail.com>
2021-05-31 12:36:58 -0500

    Fixed output, but it is practically serial (not parellel)

--- fftw3-mpi_helloworld.c
+++ fftw3-mpi_helloworld.c
@@ -2,12 +2,23 @@
 #include <stdio.h>
 #include <math.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 
 void print_complex_array(const fftw_complex arr[], const int n){
-    int i;
-    for(i=0; i < n; i++)
+    for(int i=0; i < n; i++)
         printf("%f %f\n", arr[i][0],arr[i][1]);
+}
+
+void print_to_file(const fftw_complex arr[], const int n){
+    FILE * fptr;
+    fptr = fopen("test.txt" ,"a");
+    if (fptr == 0){
+        printf("Error--file could not be opened.\n");
+        exit (1);
+    }
+    for(int i=0; i < n; i++)
+        fprintf(fptr, "%f %f\n", arr[i][0],arr[i][1]);
+    fclose(fptr);
 }
 
 void load_data(fftw_complex in[], int N, int n, int start_read){
@@ -40,23 +51,14 @@ void load_data(fftw_complex in[], int N, int n, int start_read){
 }
 
 int main(int argc, char ** argv){
+    // REMOVING FILE TO DELETE PREVIOUS DATA
+    remove("test.txt");
+    
     // DECLARING NECESSARY VARIABLES
     const ptrdiff_t N = 14;
-    //fftw_complex *in;//, *out;
     fftw_plan p;
     ptrdiff_t alloc_local, local_ni, local_no, local_i_start, local_o_start;
     
-    // ALLOCATING AND LOADING TOY INPUT
-    // It must be kind of pointless to be working in paralel and saving the whole sample in all processes but this is just for practice (the next 2 lines):
-    /*in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-    //out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
-    double x_values[N];
-    for(int i = 0; i<N; i+=1){
-        x_values[i] = i;
-        in[i][1] = 0;
-        in[i][0] = sin(2*M_PI*x_values[i] / 32);  // 32 - 256Hz   ..I think
-        in[i][0] += sin(2*M_PI*x_values[i] / 64); // 64 - 128Hz   ..I think
-    }*/
 
     MPI_Init(&argc, &argv);
     fftw_mpi_init();
@@ -68,7 +70,7 @@ int main(int argc, char ** argv){
                                         FFTW_FORWARD, FFTW_ESTIMATE,
                                         &local_ni, &local_i_start,
                                         &local_no, &local_o_start);
-    printf("I am process %d | local_no = %td | local_o_start = %td    || alloc_local = %td\n", rnk+1, local_ni, local_i_start, alloc_local);
+    //printf("I am process %d | local_no = %td | local_o_start = %td    || alloc_local = %td\n", rnk+1, local_ni, local_i_start, alloc_local);
     
     // ALLOCATING DATA (at individual processes)
     fftw_complex *local_in, *local_out;
@@ -82,34 +84,33 @@ int main(int argc, char ** argv){
 
     
     // THEN WE HAVE TO INITIALLIZE THE DATA (in each individual process)
-    //for (i=0;i<local_ni;i++)
-    //    local_in[i][0] = in[local_i_start+i][0];
     load_data(local_in, N, local_ni, local_i_start);
 
-    MPI_Barrier(MPI_COMM_WORLD);
+    //MPI_Barrier(MPI_COMM_WORLD);
 
     // PRINTING THE INPUT (of each process) (in order)
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-    for(int i=0;i<world_size;i++){
+    /*for(int i=0;i<world_size;i++){
         if(i==rnk)
             print_complex_array(local_in, local_ni);
         MPI_Barrier(MPI_COMM_WORLD);
-    }
+    }*/
 
-    MPI_Barrier(MPI_COMM_WORLD);
+    /*MPI_Barrier(MPI_COMM_WORLD);
     printf("\n");
-    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);*/
 
     // NOW THE TRANSFORM
     fftw_execute(p);
     
     // PRINTING THE OUTPUT (of each process) (in order)
-    //print_complex_array(local_out, local_ni); // use just this line but it won't be in order
     for(int i=0;i<world_size;i++){
-        sleep(1);
-        if(i==rnk)
-            print_complex_array(local_out, local_no);
+        //sleep(1);
+        if(i==rnk){
+            //print_complex_array(local_out, local_no);
+            print_to_file(local_out, local_no);
+        }
         MPI_Barrier(MPI_COMM_WORLD);
     }
     
@@ -120,27 +121,10 @@ int main(int argc, char ** argv){
         
     return 0;
 }
-/*
 
+/*
 We also have to link mpich with -lmpich:
 gcc fftw3-mpi_helloworld.c -lfftw3_mpi -lfftw3 -lm -lmpich -Wall
 and then.. :  (where n is the number of processes)
 mpiexec -n 4 ./a.out 
-
-TO DO:
-- Check why results across multiple process (exampes: 1, 2, 4) are different (kind of unnordered towards the end)
-    - I did some experiments using sleep() function and the problem seems to be happening
-      while printing the output. If the programm sleep() for 1 second before printing each process'
-      output, it prints equal across all n's tested (1,2,3,4,5). Of course we dont want to sleep().
-        - One solution might be outputing to different to one file or different files (not to terminal)
-        - It might get solved if we send output to file: mpiexec -n 4 ./a.out  ?
-
-    - Cancelled : ~Maybe work with a bigger sample (FFTW specifies in their documentation that
-                                       their algorithms are not deterministic and
-                                       thay may vary slightly across identic runs,
-                                       our sample is of 15 so maybe we need a bigger one,
-                                       though it worked perfectly with a 128 sample)~
-        - Maybe not cancelled: maybe printing is so fast because the sample is very little, so maybe there's
-          no time to finish printing on screen one process' output by the time the other print signal has been
-          sent so now there are two printing task at the same time. Maybe?
 */
