4f367c2c639f29add754f049ab5700afc935d796
jruiz971 <juanluisruiz971@gmail.com>
2021-06-14 22:34:29 -0500

    Program to read binary file and plot samples
    Read binary file from path and plot samples

---
+++ plot_samples.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+def plot_file(INPUT_FILE_PATH):
+    samples = np.fromfile(INPUT_FILE_PATH, np.complex64)
+    plt.plot(np.real(samples), np.imag(samples), '.')
+    plt.grid(True)
+    plt.show()
+
+if __name__=="__main__":
+    plot_file( sys.argv[1] ) #Center frequency (radio station [Hz])
+    print(sys.argv[1])
+    """
+    Run:
+        python3 plot_samples.py <INPUT_FILE_PATH>
+    Run Example:
+        python3 plot_samples.py out.iq
+    """
